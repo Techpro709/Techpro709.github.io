721934864c2bb668728c387091ad464a201221f6
Techpro709 <61668117+Techpro709@users.noreply.github.com>
2020-05-16 22:53:21 -0700

    secret
    No body touch Secret.js

---
+++ secret.js
@@ -0,0 +1,16 @@
+
+var secret1 = "I love Hafsa";
+var secret2 = "Hafsa is my best friend";
+var secret3 = "";
+var secret4 = "";
+var secret5 = "";
+var secret6 = "";
+var secret7 = "";
+var secret8 = "";
+var secret9 = "";
+var secret10 = "";
+var secret11  = "";
+var secret12 = "";
+var secret13  = "";
+var secret14  = "";
+var secret15  = "";
